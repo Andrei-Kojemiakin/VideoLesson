5d8122a4c0174a9f84ad82be8dd9cd055cba4328
Andrei Kojemiakin <kav231@yandex.ru>
2021-02-14 13:13:01 +0300

    Up date 13.12 14.02.2021

--- src/Lesson5/Car2.java
+++ src/Lesson5/Car2.java
@@ -2,19 +2,40 @@ package Lesson5;
 
   public class Car2 {
 
+    String color;
+    String engine;
+    int speed;
+
+    int gaz(int skorost) {
+      speed += skorost;
+      return speed;
+    }
+
+    int tormoz(int skorost) {
+      speed -= skorost;
+      return speed;
+    }
+
+    void showInfo() {
+      System.out.println(" cvet: " + color + " motor: " + engine + " skorost: " + speed + " k/h.");
+
+    }
+  }
 
 
-
-
-
-
-
-
-
-
-
+  class Car2test {
     public static void main(String[] args) {
-        // write your code here
+      Car2 c1 =new Car2();
+      c1.color ="White";
+      c1.engine = "V6";
+      c1.speed = 60;
+
+      c1.showInfo();
+      c1.gaz(20);
+      c1.showInfo();
+      c1.tormoz(80);
+      c1.showInfo();
     }
-    }
-}
+
+  }
+
