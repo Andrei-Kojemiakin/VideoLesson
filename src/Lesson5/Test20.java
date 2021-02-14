9f7be333e9712dcb8281cc077b719cb053499773
Andrei Kojemiakin <kav231@yandex.ru>
2021-02-14 14:19:12 +0300

    Up date 14.12 14.02.2021 Add claes Test20 and Test21. Also add method to class Test21.

--- src/Lesson5/Test20.java
+++ src/Lesson5/Test20.java
@@ -1,4 +1,24 @@
 package Lesson5;
 
 public class Test20 {
+
+    int summa(int a, int b, int c){
+        int result = a + b + c;
+        return result;
+    }
+}
+
+
+class  Test21{
+
+    public static void main(String[] args) {
+
+        Test20 t = new Test20();
+        int summaTrexChisel = t.summa(1,2,3);
+        System.out.println(summaTrexChisel);
+        System.out.println(t.summa(5,10,15));
+    }
+
+
+
 }
