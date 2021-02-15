f82e6801f81d989391c995c62c8dcd72be88368b
Andrei Kojemiakin <kav231@yandex.ru>
2021-02-15 15:50:24 +0300

    Up date 15.49 15.02.2021 Home work lesson5 part-3 Фсё. Я сделяль))

---
+++ src/HomeWork/Lesson5/Employee.java
@@ -0,0 +1,38 @@
+package HomeWork.Lesson5;
+
+public class Employee {
+    Employee(int nomer, String familiya, double zarplata, String departament) {
+
+    id=nomer;
+    surname=familiya;
+    salary=zarplata;
+    department=departament;
+    }
+
+    String surname,department;
+    int id;
+    double salary;
+
+
+    double wagesIncrease() {
+        salary= salary*2;
+        return salary;
+
+    }
+
+    void showInfo(){
+        System.out.println("Nomer: "+id+" Familiya: "+surname+" ZP: "+salary+" Departament: "+department);
+}   }
+
+  class EmployeeTest{
+
+    public static void main(String[] args) {
+        Employee e1=new Employee(12,"Ivanov", 234,"Dvorniki");
+
+        e1.showInfo();
+        e1.wagesIncrease();
+        e1.showInfo();
+
+
+    }
+}
