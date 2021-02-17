bfcf2abb160f855a6dec667b30bb6ce36b3cef9d
Andrei Kojemiakin <kav231@yandex.ru>
2021-02-17 18:53:10 +0300

    Added task from Igor.

---
+++ src/TasksFromIgor/FealdDefine.java
@@ -0,0 +1,34 @@
+package TasksFromIgor;
+
+
+// Создать класс в нём два поля Int и String. Создать объект этого класса в котором значение переменной Int =25 Strintg= "JAVA"
+
+public class FealdDefine {
+
+   // public FealdDefine(){
+
+    //}
+    public FealdDefine(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    int a;
+    String b;
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+}
---
+++ src/TasksFromIgor/New.java
@@ -0,0 +1,35 @@
+package TasksFromIgor;
+
+import TasksFromIgor.FealdDefine;
+
+public class New {
+
+    // Принимет два чесла типа Int Возвращает их сумму
+ public static int sumTwoNumbers(int a, int b){
+
+     return a+b;
+
+ }
+
+    public static void main(String[] args) {
+
+        FealdDefine fealdDefine1= new FealdDefine(12,"Java12");
+       // fealdDefine1.setA(25);
+       // fealdDefine1.setB("Java");
+
+        System.out.println(fealdDefine1.getA());
+        System.out.println(fealdDefine1.getB());
+
+    }
+// Создать класс в нём два поля Int и String. Создать объект этого класса в котором значение переменной Int =25 Strintg= "JAVA"
+
+
+  class FaldDefine {
+
+
+// Создать два класса в первом классе объявить 3 поля во вором объявить 2 поля. В Main создать 2 объекта первого класса два объекта второго класса с одинаковыми полями.
+// Сравнить объекты первого и второго класса между собой.
+}
+
+
+}
